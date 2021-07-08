9cf116e4e18362bb757f0af45c70a67f281cd6c7
Jean Vicencio <alexisvicencio92@gmail.com>
2021-07-08 18:33:21 -0400

    subida

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn djangoProject3.wsgi:application --log-file -
--- Servicios/admin.py
+++ Servicios/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
+from Servicios.models import *
 
 # Register your models here.
+
 from .models import Servicio
 class ServicioAdmin(admin.ModelAdmin):
     readonly_fields = ('created','updated')
 admin.site.register(Servicio, ServicioAdmin)
+
